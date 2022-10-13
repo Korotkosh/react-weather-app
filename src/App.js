825ae5b496980bd3eaa63b5cd03141f7e2076c48
Korotkosh <108016188+Korotkosh@users.noreply.github.com>
2022-10-13 12:09:58 +0300

    added GitHub link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div>
+      <footer>
+        This project was coded by Antonina Korotkosh{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Korotkosh/react-weather-app"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          open-sourced on GitHub
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
-
-export default App;

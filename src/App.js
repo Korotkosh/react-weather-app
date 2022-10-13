c6a95a3204130b04841b2523d958146b35301af9
Korotkosh <108016188+Korotkosh@users.noreply.github.com>
2022-10-13 13:01:26 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div>
-      <footer>
-        This project was coded by Antonina Korotkosh{" "}
-        <a
-          href="https://github.com/Korotkosh/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+    <div className="App">
+      <div className="container">
+        <footer>
+          This project was coded by Antonina Korotkosh{" "}
+          <a
+            href="https://github.com/Korotkosh/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

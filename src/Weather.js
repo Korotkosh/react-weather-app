cd084767502d2fcb4b9992241f8b8b39597aab94
Korotkosh <108016188+Korotkosh@users.noreply.github.com>
2022-10-13 20:31:05 +0300

    used corect icon url

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
       description: response.data.weather[0].description,
-      icomUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+      icomUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
 

3980eb37f5bd5933832c7abde78c6581ab8e3548
Korotkosh <108016188+Korotkosh@users.noreply.github.com>
2022-10-14 17:56:21 +0300

    fixed the code

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,7 +15,7 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <WeatherIcon code={props.data.icon} />
+          <WeatherIcon code={props.data.icom} />
 
           <WeatherTemperature celsius={props.data.temperature} />
         </div>

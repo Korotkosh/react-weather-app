1dda28fb838a5c69be38fabe3d41aed4cd378a2c
Korotkosh <108016188+Korotkosh@users.noreply.github.com>
2022-10-23 14:40:13 +0300

    added API to weatherForecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,15 +1,25 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
+import axios from "axios";
 import "./WeatherForecast.css";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {}
+
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.length(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
           <div className="WeatherForecast-day">Fri</div>
 
-          <WeatherIcon code="01d" />
+          <WeatherIcon code="01d" size={36} />
 
           <div className="WeatherForecast-temp">
             <span className="max">10 °C</span> |{" "}
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,7 +17,7 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <WeatherIcon code={props.data.icom} />
+          <WeatherIcon code={props.data.icom} size={84} />
 
           <WeatherTemperature celsius={props.data.temperature} />
         </div>
